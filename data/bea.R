e9806ee9af6ebef6196a81df224172ecc582285e
Unknown <jake.j.is@gmail.com>
2017-09-10 13:11:12 -0700

    finish bea in /data

--- data/bea.R
+++ data/bea.R
@@ -1,127 +1,101 @@
 #-------------------------------------------------------------------------------
-# Preample
+# downloand and unzip
 #-------------------------------------------------------------------------------
-rm(list = ls())
-source("~/GoogleDrive/Research/Congress/congress.Rprofile")
+td <- tempdir()
 
-url1 <- "https://www.bea.gov/regional/zip/gsp/gsp_naics_all.zip"
-url2 <- "https://www.bea.gov/regional/zip/gsp/gsp_sic_all.zip"
+pag <- 'https://www.bea.gov/regional/zip/'
 
-#-------------------------------------------------------------------------------
-# State info
-#-------------------------------------------------------------------------------
-states <- read.csv(paste0(d$congress, "Data/us_state_info.csv"))
+# naics
+download.file(str_c(pag, 'gsp/gsp_naics_all.zip'), str_c(td, '/naics.zip'))
+unzip(str_c(td, '/naics.zip'), file = 'gsp_naics_all.csv', exdir = td)
+dat1 <- read_csv(str_c(td, '/gsp_naics_all.csv'))
 
-#-------------------------------------------------------------------------------
-# NAICS 1997-2013
-#-------------------------------------------------------------------------------
-naics <- read.csv(paste0(d$bea, "raw_files/gsp_naics_all.csv"))
+# sic
+download.file(str_c(pag, 'gsp/gsp_sic_all.zip'), str_c(td, '/sic.zip'))
+unzip(str_c(td, '/sic.zip'), file = 'gsp_sic_all.csv', exdir = td)
+dat2 <- read_csv(str_c(td, '/gsp_sic_all.csv'))
 
-# Keep only states
-naics <- subset(naics, GeoName %in% states$statedscr)
+# gsi/population
+download.file(str_c(pag, '/spi.zip'), str_c(td, '/spi.zip'))
+unzip(str_c(td, '/spi.zip'), file = 'SA1_1929_2016.csv', exdir = td)
+dat3 <- read_csv(str_c(td, '/SA1_1929_2016.csv'))
 
-# Keep All-industry values
-naics <- subset(naics, IndustryId == 1)
-
-# Keep GSP values
-naics <- subset(naics, ComponentId == 200)
-
-# Get FIPS
-naics <- merge(naics, states, by.x = "GeoName", by.y = "statedscr")
+unlink(td)
 
 #-------------------------------------------------------------------------------
-# SIC 1963-1996
+# naics 1997 to 2016
 #-------------------------------------------------------------------------------
-sic <- read.csv(paste0(d$bea, "raw_files/gsp_sic_all.csv"))
-  
-# Keep only states
-sic <- subset(sic, FIPS > 0 & FIPS < 57)
 
-# Keep All-industry values
-sic <- subset(sic, IndustryId == 1)
-
-# Drop overlaping year
-sic <- subset(sic,select=-c(gsp1997))
-
-sic <- rename(sic, fips = FIPS)
-
-#-------------------------------------------------------------------------------
-# Merge NAICS and SIC
-#-------------------------------------------------------------------------------
-bea.gsp <- sic %>%
-  merge(naics, by = "fips") %>%
-  melt(id = "fips") %>%
-  mutate(variable = as.character(variable)) %>%
-  subset(substr(variable, 1, 3) == "gsp" | substr(variable, 1, 1) == "X") %>%
+# add fips (na's correspond to notes)
+naics <- dat1 %>%
   mutate(
-    variable = gsub("gsp", "", variable),
-    variable = gsub("X", "", variable),
-    variable = as.numeric(variable),
-    value = as.numeric(value)*1E6
+    fips = as.integer(str_sub(GeoFIPS, 1, 2))
     ) %>%
-  setNames(c("fips", "year", "gsp"))
+  filter(!is.na(fips))
+
+# reshape to wide and convert year
+naics <- gather(naics, year, value, `1997`:`2016`) %>%
+  mutate(
+    value = as.numeric(value),
+    year  = as.integer(year)
+  )
 
 #-------------------------------------------------------------------------------
-# GSI and population data
+# sic 1963 to 1996
 #-------------------------------------------------------------------------------
-gsi <- read.csv(paste0(d$bea, "/raw_files/SA1_1929_2014.csv"))
 
-# Keep only states
-gsi <- subset(gsi, GeoName %in% states$statedscr)
-
-# Get FIPS
-gsi <- merge(gsi, states, by.x = "GeoName", by.y = "statedscr")
-
-# Separate data (income)
-gsi.inc <- subset(gsi, LineCode == 1)
-
-# Reshape
-gsi.inc <- gsi.inc %>%
-  melt(id = "fips") %>%
-  subset(substr(variable, 1, 1) == "X") %>%
+# add fips (na's correspond to notes)
+sic <- dat2 %>%
   mutate(
-    variable = gsub("X", "", variable),
-    variable = as.numeric(variable),
-    value = as.numeric(value)*1000
-    ) %>%
-  setNames(c("fips", "year", "gsi"))
-
-# Separate data (population)
-gsi.pop <- subset(gsi, LineCode == 2)
-
-# Reshape
-gsi.pop <- gsi.pop %>%
-  melt(id = "fips") %>%
-  subset(substr(variable, 1, 1) == "X") %>%
-  mutate(
-    variable = gsub("X", "", variable),
-    variable = as.numeric(variable),
-    value = as.numeric(value)
+    fips = as.integer(str_sub(GeoFIPS, 1, 2))
   ) %>%
-  setNames(c("fips", "year", "pop.bea"))
+  filter(!is.na(fips)) %>%
+  select(-`1997`)
 
-#-------------------------------------------------------------------------------
-# Merge everything together
-#-------------------------------------------------------------------------------
-bea.all <- gsi.inc %>%
-  merge(gsi.pop, by = c("fips", "year")) %>%
-  merge(bea.gsp, by = c("fips", "year"), all.x = T, all.y = T) %>%
-  unique
-
-#-------------------------------------------------------------------------------
-# Convert to real values
-#-------------------------------------------------------------------------------
-load(paste0(d$bls, "bls_cpi.Rda"))
-
-bea.income <- merge(bea.all, cpi, by = "year", all.x = T, all.y = F) %>%
+# reshape to wide and convert year
+sic <- gather(sic, year, value, `1963`:`1996`) %>%
   mutate(
-    rgsp = gsp / (cpi/100),
-    rgsi = gsi / (cpi/100)
-    ) %>%
-  unique %>%
-  select(-cpi)
+    value = as.numeric(value),
+    year  = as.integer(year)
+  )
 
 #-------------------------------------------------------------------------------
-# Save
+# gsi and population data
 #-------------------------------------------------------------------------------
-save(bea.income, file = paste0(d$bea,"bea_income.Rda"))
+
+# add fips (na's correspond to notes)
+gsi <- dat3 %>%
+  mutate(
+    fips = as.integer(str_sub(GeoFIPS, 1, 2))
+  ) %>%
+  filter(!is.na(fips))
+
+# reshape to wide, convert year, drop per capita income, and drop LineCode
+gsi <- gather(gsi, year, value, `1929`:`2016`) %>%
+  mutate(
+    value = as.numeric(value),
+    year  = as.integer(year)
+  )
+
+#-------------------------------------------------------------------------------
+# manual edits
+#-------------------------------------------------------------------------------
+
+# bind gsp together, add units as a reference
+bea_gsp <- rbind(sic, naics) %>%
+  mutate(
+    units = 'millions of current dollars'
+    )
+
+# split into gsi and population tables
+bea_gsi <- filter(gsi, LineCode == 1)
+bea_pop <- filter(gsi, LineCode == 2)
+
+# ------------------------------------------------------------------------------
+# add to database
+# ------------------------------------------------------------------------------
+db <- src_sqlite("~/GoogleDrive/Projects/congress/congress.db", create = F)
+
+copy_to(db, bea_gsp, temporary = F, overwrite = T)
+copy_to(db, bea_gsi, temporary = F, overwrite = T)
+copy_to(db, bea_pop, temporary = F, overwrite = T)
