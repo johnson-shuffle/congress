3a41157e8e19274b8bd88c1fd66af67217c1727a
johnson-shuffle <jake.j.is@gmail.com>
2018-08-20 20:00:04 -0700

    get rid of scratchwork

--- scratchwork.R
+++
@@ -1,9 +0,0 @@
-z <- tbl(db_crp, sql(
-  "SELECT * FROM indivs
-  WHERE fecrecno='4122020121176554351'"
-  )) %>% 
-  collect()
-
-duplicates(x, c('cycle', 'cmteid'))
-
-db <- src_postgres('trumptown')
